20c9a1988ba50d3b19f8f1c31becd205d32c1051
jatin <jatin0700.be23@chitkara.edu.in>
2024-07-01 11:17:03 +0530

    make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
-let productsHtml ='';
+let productsHtml = '';
 
 products.forEach((product) => {
-    productsHtml+= `
+  productsHtml += `
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -14,14 +14,14 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${10*product.rating.stars}.png">
+              src="images/ratings/rating-${10 * product.rating.stars}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceCents/100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -46,11 +46,37 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
     `
 });
 
-document.querySelector('.js-products-grid').innerHTML =productsHtml;
+document.querySelector('.js-products-grid').innerHTML = productsHtml;
+
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+    console.log(cart);
+  })
+})
+
+const displayAdded = () => {
+
+}
